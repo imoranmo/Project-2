1796277fc39e9e9423987c4614cafe1c99024ad8
Luke Cole <Lukeajcole@gmail.com>
2021-09-18 10:22:29 -0700

    push for initial partial main merge

--- controllers/feedRoutes.js
+++ controllers/feedRoutes.js
@@ -1,6 +1,6 @@
 //Main feed page
 const router = require('express').Router();
-const { Rhythms, Posts } = require('../models');
+const { Rhythms, Posts, Users } = require('../models');
 
 router.get('/',async (req, res) => {
     try {
@@ -8,8 +8,14 @@ router.get('/',async (req, res) => {
       const rhythms = rhythmData.map((rhythm) => rhythm.get({ plain: true }));
       const postData = await Posts.findAll();
       const posts = postData.map((post) => post.get({ plain: true }));
+      
       res.render('feed', {rhythms, posts,
-        logged_in: req.session.logged_in });
+        logged_in: req.session.logged_in },        
+        
+        // {include: {
+        //   model: Users,
+        // }}
+      );
     } catch (err) {
       res.status(500).json(err);
     }
