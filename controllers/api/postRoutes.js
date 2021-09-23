1fd4f00a3b9b6a8e3d901228cba2c05bc495fb29
Luke Cole <Lukeajcole@gmail.com>
2021-09-22 21:44:03 -0700

    handle new comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -50,9 +50,26 @@ router.put('/updatePost/:id', async (req, res) => {
       const {title, content, date_updated, rhythm_id} = req.body;
       const updateBody = {title, content, date_updated, user_id, rhythm_id}
       
-      console.log(updateBody);
 
       await Posts.update(updateBody, { where: { id: req.params.id } });
+
+      res.status(200).end();
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+router.post('/addComment/:id', async (req, res) => {
+ 
+  try {
+      const user_id = req.session.user_id;
+      const post_id = req.params.id;
+      const {content, date_created} = req.body;
+      const updateBody = {content, date_created, user_id, post_id}
+      
+      console.log(updateBody);
+
+      await Comments.create(updateBody);
 
       res.status(200).end();
     } catch (err) {
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,66 +1,103 @@
 const commentToggleBtn = document.querySelector('#toggle-comments')
-
-const format_time =  (date) => {
-  return date.toLocaleTimeString();
-}
+const inputComment = document.querySelector('#comment')
+const commentBlock = commentToggleBtn.nextElementSibling;
 
 // The custom helper 'format_date' takes in a timestamp
 const format_date = (date) => {
   // Using JavaScript Date methods, we get and format the month, date, and year
   // We need to add one to the month since it is returned as a zero-based value
-  return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-    // We add five years to the 'year' value to calculate the end date
-    new Date(date).getFullYear()
-  }`;}
+  return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear() } at ${new Date(date).toLocaleTimeString()}`;}
 
 const commentToggleHandler = async (event) => {
   // Stop the browser from submitting the form so we can do so with JavaScript
-  event.preventDefault();
-
-  try {
-    
-      // Gather the data from the form elements on the page
+      event.preventDefault();
       const post_id = event.target.value;
-      const commentBlock = commentToggleBtn.nextElementSibling;
-      const url = '/api/posts/comments/' + post_id
-      // Send the e-mail and password to the server
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      })
 
-     const commentArr = await response.json()
-      console.log(commentArr);
+      loadComments(post_id);
+
+
       
-      commentBlock.innerHTML = '<h3>Comments:</h3>';
-
-      if (commentBlock.classList.contains('hidden')) {
-        for (i=0; i < commentArr.length; i++){
-   
-           const {user_name} = commentArr[i].user;
-           const {content, date_created} = commentArr[i]
-           let commentEl = document.createElement("div");
-           let commentUser = document.createElement("h3");
-           let commentP = document.createElement("p");
-            const newDate = format_date(date_created) //+ " " + format_time(date_created)
-           commentUser.innerText = `By: ${user_name} on ${newDate}`
-           commentP.innerText = content
-           commentEl.append(commentUser, commentP);
-           commentBlock.append(commentEl);
-        }
-      }
-
       commentBlock.classList.toggle("hidden");
       if (commentBlock.classList.contains("hidden")) {
         commentToggleBtn.innerHTML = "Show Comments"
       } else {
         commentToggleBtn.innerHTML = "Hide Comments"
       }
-      
-  } catch {
-
-  }
+  
     };
 
+    const loadComments = async (post_id) => {
+      // Gather the data from the form elements on the page
+      try{
+          
+          const url = '/api/posts/comments/' + post_id
+          // Send the e-mail and password to the server
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+          })
+
+        const commentArr = await response.json()
+          console.log(commentArr);
+        
+
+          commentBlock.innerHTML = '';
+
+            for (i=0; i < commentArr.length; i++){
+
+              const {user_name} = commentArr[i].user;
+              const {content, date_created} = commentArr[i]
+              let commentEl = document.createElement("div");
+              let commentUser = document.createElement("h3");
+              let commentP = document.createElement("p");
+              //  const newTime = format_time(date_created);
+              const newDate = format_date(date_created);
+              
+
+              commentEl.className= 'max-w-4xl px-10 py-4 mx-auto mb-2 bg-gray-200 rounded-lg shadow-md';
+              commentUser.innerText = `${user_name} on ${newDate}`
+              commentUser.className = 'italic text-grey-200 text-sm'
+              commentP.className = 'font-semibold'
+              commentP.innerText = content
+              commentEl.append(commentUser, commentP);
+              commentBlock.append(commentEl);
+            }
+          } catch (err) {
+            console.log(err)
+          }
+    }
+
+    const postComment = async (event) => {
+          
+      const post_id = commentToggleBtn.value;
+      const url = '/api/posts/addComment/' + post_id
+      const date_created = new Date().toISOString().slice(0, 19).replace('T', ' ');
+      const content = event.target.value
+
+      const newComment = {content, date_created};
+
+      if (content && date_created) {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(newComment),
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      if (response.ok) {
+        event.target.value = ""
+        loadComments(post_id)
+      } else {
+        alert('Failed to post comment');
+      }
+    }
+  }
 
     commentToggleBtn.addEventListener('click', commentToggleHandler);
+
+    inputComment.addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+          postComment(event)
+      }
+  });
+
+
